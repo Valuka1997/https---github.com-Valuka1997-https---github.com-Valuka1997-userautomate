3b0af160296e883985f62045119333d146d6d6de
Valuka gimhan <valukagimhan@gmail.com>
2022-08-06 20:24:35 +0530

    v1

---
+++ users.sh
@@ -0,0 +1,26 @@
+# useradd -c #comment
+# -e #expire date
+# -p #password
+# -u userid
+
+# groupadd -p #password
+
+#!/bin/bash
+
+#Define the string value
+text="Welcome to LinuxHint"
+
+# Set space as the delimiter
+IFS=' '
+
+#Read the split words into an array based on space delimiter
+read -a strarr <<< "$text"
+
+#Count the total words
+echo "There are ${#strarr[*]} words in the text."
+
+# Print each value of the array by using the loop
+for val in "${strarr[@]}";
+do
+  printf "$val\n"
+done
