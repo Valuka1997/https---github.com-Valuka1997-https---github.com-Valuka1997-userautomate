44fa52bdaf5510d4f0b46829db8234d3d716c6d7
Valuka gimhan <valukagimhan@gmail.com>
2022-09-05 04:49:59 +0530

    ***

--- useradd.sh
+++ useradd.sh
@@ -1,32 +1,8 @@
 #!/bin/bash
 
-# useradd -c #comment
-# -e #expire date
-# -p #password
-# -u userid
-
-# groupadd -p #password
-
-g="Sales HR Finance Personnel CEO Shipping Managers"
-groupadd
-
-
-# input = "path to file"
+input = "path to file"
 while read -r line
     do
         read first last uid role pass<<< ${line}
         $(useradd ${uid} -c ${role} -p ${pass} )
     done < "$input"
-
-
-
-x="abju   aspfj   akijfae   adgadf  qeahsrre"
-
-read first last uid role pass<<< ${line}
-echo $first
-echo $last
-echo $uid
-echo $role
-echo $pass
-
-#new comment
